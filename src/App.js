f8ef9035b6d0aa7fb975c42751e760b85442d269
HaoTong Zhi <s3637727@student.rmit.edu.au>
2018-11-25 15:08:06 +1100

    create APP and reset CSS

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component , Fragment } from 'react';
+import {GlobalStyle} from './style';
+
+
+class App extends Component {
+  render() {
+    return (
+        <Fragment>
+          <GlobalStyle />
+          <div>zhi</div>
+        </Fragment>
+     
+    );
+  }
+}
+
+export default App;
