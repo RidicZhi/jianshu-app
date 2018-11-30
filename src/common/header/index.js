9f0c236326ffc7f6380c80c332e01f62de4e3662
HaoTong Zhi <s3637727@student.rmit.edu.au>
2018-11-30 18:01:11 +1100

    get "hot search list" by axios

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CSSTransition } from 'react-transition-group';
 import { 
@@ -18,83 +18,88 @@ import {
 } from './style';
 import { actionCreators } from './store';
 
-const getListArea = (show)=>{
-  if (show) {
-    return (
-      <SearchInfo >
-            <SearchInfoTitle>
-              Hot Search
-              <SearchInfoSwitch>refresh</SearchInfoSwitch>
-            </SearchInfoTitle>
+class Header extends Component {
 
-            <SearchInfoList>
-              <SearchInfoItem>live</SearchInfoItem>
-              <SearchInfoItem>sport</SearchInfoItem>
-              <SearchInfoItem>study</SearchInfoItem>
-              <SearchInfoItem>coffee</SearchInfoItem>
-              <SearchInfoItem>pet</SearchInfoItem>
-            </SearchInfoList>
-          </SearchInfo>
+  getListArea = ()=>{
+    const { focused, list} = this.props;
+    if (focused) {
+      return (
+        <SearchInfo >
+              <SearchInfoTitle>
+                Hot Search
+                <SearchInfoSwitch>refresh</SearchInfoSwitch>
+              </SearchInfoTitle>
+  
+              <SearchInfoList>
+                {
+                  list.map((item) =>{
+                    return <SearchInfoItem key={item}>{item}</SearchInfoItem>
+                  })
+                }
+              </SearchInfoList>
+            </SearchInfo>
+      )
+    }else{
+      return null;
+    }
+  }
+  render(){
+    const { focused, handleInputFocus, handleInputBlur } = this.props;
+    return (
+      <HeaderWrapper>
+        <Logo />
+  
+        <Nav>
+          <NavItem className = 'left active'>Home</NavItem>
+          <NavItem className = 'left'>Download App</NavItem>
+          <NavItem className = 'right'>Login</NavItem>
+          <NavItem className = 'right'>
+            <i className="iconfont icon-xxx">&#xe636;</i>
+          </NavItem>
+  
+          <SearchWrapper>
+            <CSSTransition
+              in={focused} 
+              timeout={200} 
+              classNames="slide"
+            >
+              <NavSearch 
+                className = {focused ? 'focused' : ''}
+                onFocus = {handleInputFocus}
+                onBlur = {handleInputBlur}
+                ></NavSearch>
+            </CSSTransition>
+            <i className = {focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+            {this.getListArea()}
+          </SearchWrapper>
+  
+        </Nav>
+  
+        <Addition>
+          <Button className = 'compose'>
+            <i className = "iconfont icon-xxx">&#xe615;</i>
+            Compose
+          </Button>
+          <Button className = 'reg'>Register</Button>
+        </Addition>
+  
+      </HeaderWrapper>
     )
-  }else{
-    return null;
   }
 }
-
-const Header = (props) => {
-  return (
-    <HeaderWrapper>
-      <Logo />
-
-      <Nav>
-        <NavItem className = 'left active'>Home</NavItem>
-        <NavItem className = 'left'>Download App</NavItem>
-        <NavItem className = 'right'>Login</NavItem>
-        <NavItem className = 'right'>
-          <i className="iconfont icon-xxx">&#xe636;</i>
-        </NavItem>
-
-        <SearchWrapper>
-          <CSSTransition
-            in={props.focused} 
-            timeout={200} 
-            classNames="slide"
-          >
-            <NavSearch 
-              className = {props.focused ? 'focused' : ''}
-              onFocus = {props.handleInputFocus}
-              onBlur = {props.handleInputBlur}
-              ></NavSearch>
-          </CSSTransition>
-          <i className = {props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
-          {getListArea(props.focused)}
-        </SearchWrapper>
-
-      </Nav>
-
-      <Addition>
-        <Button className = 'compose'>
-          <i className = "iconfont icon-xxx">&#xe615;</i>
-          Compose
-        </Button>
-        <Button className = 'reg'>Register</Button>
-      </Addition>
-
-    </HeaderWrapper>
-  )
-}
-
 
 const mapStateToProps = (state) => {
   return {
     //focused: state.get('header').get('focused')
-    focused: state.getIn(['header','focused'])
+    focused: state.getIn(['header','focused']),
+    list: state.getIn(['header','list'])
   }
 }
 
 const mapDispatchToProps = (dispatch) =>{
   return {
     handleInputFocus(){
+      dispatch(actionCreators.getList());
       dispatch(actionCreators.searchFocusAction());
     },
     handleInputBlur(){
--- src/common/header/store/actionCreator.js
+++ src/common/header/store/actionCreator.js
@@ -1,9 +1,26 @@
 import { constants } from './';
+import axios from 'axios';
+import { fromJS } from 'immutable';
 
 export const searchFocusAction = () => ({
   type: constants.SEARCH_FOCUS
-})
+});
 
 export const searchBlurAction = () => ({
   type: constants.SEARCH_BLUR
-})
+});
+
+export const getList = () =>{
+  return (dispatch) =>{
+    axios.get('/api/headerList.json').then((res) => {
+      dispatch(changeList(res.data.data));
+    }).catch(() => {
+      console.log("error")
+    })
+  }
+};
+
+const changeList = (data) => ({
+  type: constants.CHANGE_LIST,
+  data: fromJS(data)
+});
