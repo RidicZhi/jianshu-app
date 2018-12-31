40b4985edc1b51a58585454ae4cece5eb024cb55
HaoTong Zhi <s3637727@student.rmit.edu.au>
2019-01-01 03:56:17 +1100

    react router/ home page and detail page components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component , Fragment } from 'react';
 import {GlobalStyle} from './style';
-import {GlobalStyleIcon} from './statics/iconfont/iconfont'
-import Header from './common/header'
+import {GlobalStyleIcon} from './statics/iconfont/iconfont';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './common/header';
+import Home from './pages/home';
+import Detail from './pages/detail';
 import store from './store'
 
 
@@ -14,6 +17,12 @@ class App extends Component {
           <GlobalStyle />
           <GlobalStyleIcon />
           <Header />
+          <BrowserRouter>
+            <div>  
+              <Route path='/' exact component={Home}></Route>
+              <Route path='/detail' exact component={Detail}></Route> 
+            </div>  
+          </BrowserRouter>
         </Fragment>
       </Provider>
     );
