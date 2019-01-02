8e61b8c59220fa355ff4dfd7b3958d5734a39522
HaoTong Zhi <s3637727@student.rmit.edu.au>
2019-01-02 17:22:39 +1100

    homepage-articleList style/connect store

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,11 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { ListItem,ListInfo } from '../style';
+import { connect } from 'react-redux';
 
 class List extends Component{
   render(){
+    const{ list } = this.props;
     return (
-      <div>List</div>
+      <Fragment>
+        {
+          list.map((item)=>(
+            <ListItem key={item.get('id')}>
+              <img className='pic' src={item.get('imgUrl')} alt='img' />
+              <ListInfo>
+                <h3 className='title'>{item.get('title')}</h3>
+                <p className='desc'>{item.get('desc')}</p>
+              </ListInfo>
+            </ListItem>
+            )
+          )
+        }
+      </Fragment>
     )
   }
 }
 
-export default List;
+const mapState = (state) => {
+  return{
+    list: state.getIn(['home','articleList'])
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return{
+
+  }
+}
+
+export default connect(mapState,mapDispatch)(List);
